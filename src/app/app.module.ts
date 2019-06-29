3546c477eadf09a23ef23f6a387d1406a1a9a5bd
usmankhanbrainplow <35483640+usmankhanbrainplow@users.noreply.github.com>
2019-06-29 16:11:05 +0500

    jkjkl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { CommonComponent } from './common/common.component';
 import { ViewpageComponent } from './viewpage/viewpage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ViewpageComponent } from './viewpage/viewpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
